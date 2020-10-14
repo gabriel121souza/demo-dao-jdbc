acdc01746da9dafa0b2fa3cc6394684a0e09dd14
gabriel121souza <gabriel121souza@gmail.com>
2020-10-13 22:41:10 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		
 		Department obj = new Department(2, "Books");
-		
-		System.out.println(obj.toString());
+		Seller seller = new Seller(22, "gariel","gabriel@gmail.com", new Date(), (double) 3500, obj);
+		System.out.println(seller);
 	}
 }
